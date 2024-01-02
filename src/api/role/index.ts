cd5dad57eec69e131722c0031045a9791d221095
Dương Vĩnh Phúc Đại <97826393+PanDaIT03@users.noreply.github.com>
2024-01-02 13:43:11 +0700

    Update index.ts

--- src/api/role/index.ts
+++ src/api/role/index.ts
@@ -1,7 +1,7 @@
-import { collection, getDocs, query } from "firebase/firestore";
+import { addDoc, collection, deleteDoc, doc, getDocs, query, setDoc } from "firebase/firestore";
 
 import { fireStoreDatabase } from "~/config/firebase";
-import { IRole } from "~/types/RoleType";
+import { IRole, Role } from "~/types/RoleType";
 
 export const getRoles = async () => {
   const queryStmt = query(collection(fireStoreDatabase, "roles"));
@@ -16,15 +16,30 @@ export const getRoles = async () => {
 };
 
 export const getListRole = async () => {
-  const queryStmt = query(collection(fireStoreDatabase, "roles"));
-  const querySnapshot = await getDocs(queryStmt);
+  const q = query(collection(fireStoreDatabase, 'roles'));
+  const querySnapshot = await getDocs(q);
 
-  return querySnapshot.docs.map((doc) => ({
+  return querySnapshot.docs.map(doc => ({
     docId: doc.id,
     role: doc.data().role,
     name: doc.data().name,
     description: doc.data().description,
     functionalsId: doc.data().functionalsId,
-    allowDelete: doc.data().allowDelete,
+    allowDelete: doc.data().allowDelete
   }));
-};
+}
+
+export const deleteRoleAPI = async (id: string) => {
+  await deleteDoc(doc(fireStoreDatabase, 'roles', `${id}`));
+}
+
+export const updateRoleById = async (role: Omit<Role, 'docId'> & { docId?: string }) => {
+  const { docId } = role;
+  delete role.docId;
+
+  await setDoc(doc(fireStoreDatabase, 'roles', `${docId}`), role);
+}
+
+export const addRoleAPI = async (role: Omit<Role, 'docId'>) => {
+  await addDoc(collection(fireStoreDatabase, 'roles'), role);
+}
